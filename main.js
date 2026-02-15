2274910f1dd7f31e5d9858499d6c6213d4b47f06
ipoohh95 <ipoohh95@gmail.com>
2026-02-15 10:16:22 +0000

    feat: Initial setup of Culture Factory interior design portfolio website framework

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-// Add JS here
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+console.log("main.js loaded");
